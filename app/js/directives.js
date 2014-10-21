109d5e5973ddf4cbabaeea3dcc45eb4cfa7984dd
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-10-21 13:13:14 +0100

    E directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,10 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+var appMod = angular.module('myApp.directives', []);
+appMod.directive('custom', function() {
+   return {
+      restrict: 'E',
+      template: '<div class="jumbotron"> This is a custom directive!</div>'
+   };
+});
