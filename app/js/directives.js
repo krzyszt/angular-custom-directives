41f7c9eb0eab3965df7c4d7942e124f5ec4c0ad0
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-10-21 13:26:48 +0100

    directive restrictions

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,9 +4,35 @@
 
 
 var appMod = angular.module('myApp.directives', []);
+
 appMod.directive('custom', function() {
    return {
       restrict: 'E',
       template: '<div class="jumbotron"> This is a custom directive!</div>'
+   };
+});
+
+appMod.directive('classDir', function() {
+   return {
+      restrict: 'C',
+      template: '<div class="jumbotron"> This is a class directive!</div>'
+   };
+});
+
+appMod.directive('attrDir', function(){
+   return {
+      restrict: 'A',
+      link: function(){
+         alert('This is attribute directive!');
+      }
+   };
+});
+
+appMod.directive('commentDir', function(){
+   return {
+      restrict: 'M',
+      link: function(){
+         alert('This is comment directive!');
+      }
    };
 });
